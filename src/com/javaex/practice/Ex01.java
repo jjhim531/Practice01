fd644cdbfb980563db23e4a34c549d631d01d289
805-7 <805-7@DESKTOP-26K9A2G>
2024-06-21 11:03:00 +0900

    2024-06-21 Ex01완료

---
+++ src/com/javaex/practice/Ex01.java
@@ -0,0 +1,16 @@
+package com.javaex.practice;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		/*
+		 * [Ex01.java] 다음중 기본 자료형중 정수형자료형이 아닌 것은?
+		 *  int
+		 *  float
+		 *  byte
+		 *  long
+		 * A) float
+		 */
+	}
+
+}
---
+++ src/com/javaex/practice/Ex02.java
@@ -0,0 +1,16 @@
+package com.javaex.practice;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		/*
+		 * [Ex02.java] 
+JDK와 JRE의 차이점을 설명한 것 중 틀린 것을 찾아 수정해보세요?
+자바 프로그램을 개발하려면 JDK가 반드시 필요하다.
+자바 프로그램을 실행만 하려고 할 때는 JRE가 있으면 된다 a) no. 개발툴과 JRE모두 필요
+JDK는 개발도구 와 JRE로 구성되어 있다
+JDK에는 javac.exe가 포함되어 있지 않다. a)no. javac.exe는 컴파일러이다. 포함되어있다.
+		 */
+	}
+
+}
